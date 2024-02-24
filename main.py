18be94c39032eb617cdcc9cf44ea16f6c072beb9
Fred <frederic.juppont@skillcorner.com>
2024-02-24 14:55:25 +0100

    add typer, requirements and change main.py for typer

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import sys
+import typer
 from importlib import import_module
 
-try:
-    option = sys.argv[1]
-except IndexError:
-    sys.exit("Ajouter une option tk ou qt")
+app = typer.Typer()
 
-if option in ("tk", "qt"):
-    module = f"main_{option}"
-    import_module(module)
-else:
-    sys.exit(f"L'option {option} n'existe pas, seulement (tk ou qt)")
+
+@app.command()
+def main(option: str = typer.Argument(..., help="Choisissez entre 'tk' ou 'qt'.")):
+    if option in ("tk", "qt"):
+        module = f"main_{option}"
+        import_module(module)
+    else:
+        raise typer.Exit(f"L'option {option} n'existe pas, seulement 'tk' ou 'qt'.")
+
+
+app()
